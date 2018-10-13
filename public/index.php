8714a17838fcf8800a3d2fba8a773a46affb7b76
Holger Mueller <9jholger@gmail.com>
2018-10-13 11:25:09 -0500

    begin scaffold of pages

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Database App</title>
+</head>
+<body>
+
+<?php include "templates/header.php"; ?>
+
+ <ul>
+    <li>
+        <a href="create.php">
+        <strong>Create</strong>
+        </a>  - add a user
+    </li>
+
+    <li>
+        <a href="read.php">
+        <strong>Read</strong>
+        </a> - file a user
+    </li>
+ </ul>
+
+<?php include "templates/footer.php"; ?>
+    
+</body>
+</html>
