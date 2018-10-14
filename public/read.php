cb7888c539ce7de04f1a7e66329caba6b7828e3d
Holger Mueller <9jholger@gmail.com>
2018-10-13 22:11:26 -0500

    scaffold read page

--- public/read.php
+++ public/read.php
@@ -1,4 +1,14 @@
 
 <?php include "templates/header.php"; ?>
 
+<h2>Find user</h2>
+
+<form method="post">
+    <label for="location">Location</label>
+    <input type="text" id="location" name="location">
+    <input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Home</a>
+
 <?php include "templates/footer.php"; ?>
