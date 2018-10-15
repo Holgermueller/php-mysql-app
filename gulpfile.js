9465eb95dc96e7f44846df1577d48fe454dc333c
Holger Mueller <9jholger@gmail.com>
2018-10-14 20:15:06 -0500

    got gulpfile working

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ const connect = require('gulp-connect-php');
 const sync = require('browser-sync');
 
 gulp.task('connect-sync', () => {
-    connect.server({}, () => {
+    connect.server({
+        base: 'public'
+    }, () => {
         sync({
             proxy: '127.0.0.1:8000'
         });
     });
-    gulp.watch('**/*.php').on('change', () => {
+    gulp.watch('public/*.php').on('change', () => {
         sync.reload();
     });
 });
 
-gulp.task('default', ['connect']);
+gulp.task('default', ['connect-sync']);
